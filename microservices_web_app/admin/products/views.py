f09d856d52c67cc5786add5e546f32a92d2e276d
sheldonldev <xiadanli0320@gmail.com>
2021-05-17 10:42:22 +0800

    django rest done

--- microservices_web_app/admin/products/views.py
+++ microservices_web_app/admin/products/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import random
+
+from .models import Product, User
+from .models import User
+from .serializers import ProductSerializer
+
 
 # Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+	def list(self, request):      # GET /api/products
+		products = Product.objects.all()
+		serializer = ProductSerializer(products, many=True)
+		return Response(serializer.data)
+
+	def create(self, request):    # POST /api/products
+		serializer = ProductSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+	def retrieve(self, request, pk=None):  # GET/POST /api/products/<str:id>
+		product = Product.objects.get(id=pk)
+		serializer = ProductSerializer(product)
+		return Response(serializer.data)
+
+	def update(self, request, pk=None):    # PUT /api/products/<str:id>
+		product = Product.objects.get(id=pk)
+		serializer = ProductSerializer(instance=product, data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+	def destroy(self, request, pk=None):    # DELETE /api/request/<str:id>
+		product = Product.objects.get(id=pk)
+		product.delete()
+		return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+class UserAPIView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id
+        })
